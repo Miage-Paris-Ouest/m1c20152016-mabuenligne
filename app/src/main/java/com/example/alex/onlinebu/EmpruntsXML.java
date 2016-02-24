cd072a086dc7980b5a49de789a13c12141c2fa65
Pierre Alline <pierrealline@hotmail.fr>
2016-02-24 22:24:15 +0100

    empruntsXml

--- app/src/main/java/com/example/alex/onlinebu/EmpruntsXML.java
+++ app/src/main/java/com/example/alex/onlinebu/EmpruntsXML.java
@@ -2,6 +2,16 @@ package com.example.alex.onlinebu;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import XML.XmlParser;
+import livre.Livre;
 
 /**
  * Created by Alex on 18/02/2016.
@@ -11,9 +21,25 @@ public class EmpruntsXML extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_emprunts);
+        setContentView(R.layout.emprunt_xml);
+        String user =  this.getIntent().getStringExtra("USER");
+        TextView textUser = (TextView)findViewById(R.id.textViewEmprunts);
+        ListView listEmprunts = (ListView)findViewById(R.id.listViewEmprunts);
 
-
+        textUser.setText("Emprunts de " + user);
+        XmlParser XMl = new XmlParser();
+        Livre livre = new Livre();
+        InputStream inputStream = getResources().openRawResource(R.raw.livre);
+        //InputStreamReader ipsr = new InputStreamReader(inputStream);
+        //BufferedReader br = new BufferedReader(ipsr);
+        try {
+            livre=XMl.parse(inputStream);
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
 
     }
 }
